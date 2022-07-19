440fe4afae7b2f4625ef964a603e38675fc0e416
Shreyansh Bardia <104841983+ShreyanshBardia@users.noreply.github.com>
2022-07-19 16:13:54 +0530

    Update atten.py
    Earlier code worked for only SDSOS now this works for any college which uses NMIMS portal

--- atten.py
+++ atten.py
@@ -4,11 +4,10 @@ def attendance_calculator(username,password,since='2022-07-07',till=None):
 	import requests
 	import pandas as pd
 	with requests.session() as s:
-	  s.post("""https://portal.svkm.ac.in/usermgmt/login""",data={"jspname":"nm","username":username,"password":password},verify=False)
-	  s.get("https://portal.svkm.ac.in/SDSOS-NM-M/homepage")
-	  zz=s.get("https://portal.svkm.ac.in/SDSOS-NM-M/viewDailyAttendanceByStudent")
-	  html=(zz.text)
-
+		homepage=s.post("""https://portal.svkm.ac.in/usermgmt/login""", data ={"jspname":"nm", "username":username, "password" : password }, verify=False)
+		url=homepage.url.rstrip('homepage')
+		attendance_page=s.get(url+"viewDailyAttendanceByStudent")
+		html=(attendance_page.text)
 	df=pd.read_html(html)[0].replace({"Attendance":{"U":0,"P":1}})
 	df["Class Date"]=pd.to_datetime(df["Class Date"],dayfirst=True)
 	df.sort_values(by="Class Date",inplace=True)
@@ -17,7 +16,9 @@ def attendance_calculator(username,password,since='2022-07-07',till=None):
 	df1=df1.groupby("Subject").agg(["count","sum"])["Attendance"]
 	df1["Attendance Percentage"]=df1["sum"]/df1["count"]*100
 	df1.columns=["Total Classes","Attended","Percentage Attended"]
-	print(df1)
+	df2 = pd.DataFrame( [[df1["Total Classes"].sum(),df1["Attended"].sum(), df1['Attended'].sum()/df1['Total Classes'].sum()*100]],index=['Aggregate'],columns=df1.columns)
+	df = pd.concat( [df1, df2]) 
+	print(df)
 	
 import sys
 l=sys.argv[1:]
